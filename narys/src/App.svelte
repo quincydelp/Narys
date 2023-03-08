4c4f47ba194384d93b1c6da2166344ec254a0e91
quincydelp <quincydelp@DN0a233442.SUNet>
2023-03-08 11:23:44 -0800

    set up vercel and svelte and tailwind per chatgpt

---
+++ narys/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+  @import 'tailwindcss/base';
+  @import 'tailwindcss/components';
+  @import 'tailwindcss/utilities';
+</style>
+
